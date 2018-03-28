9f742420501c710ff05e62d8d851ab4e3f4cd0cc
Filip Pavlis <pavlis@google.com>
2018-03-28 19:50:09 +0100

    Move Terminal to androidx.
    Bug: 76692459
    Test: make Terminal and run on emulator
    Change-Id: I7dd06a956ea21b61b43a9acb1da21a90987f1fa2

--- Android.mk
+++ Android.mk
@@ -1,11 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_USE_AAPT2 := true
+
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_STATIC_ANDROID_LIBRARIES := androidx.viewpager_viewpager
 
 LOCAL_JNI_SHARED_LIBRARIES := libjni_terminal
 
